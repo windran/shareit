aa2d39917afe77ca102b03bf36b9cff69e1af3fc
Zahid Al Haris <zahidharis@gmail.com>
2021-12-25 17:30:12 +0700

    perbaikan view

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -6,26 +6,29 @@
 	<meta name="author" content="SHAREit">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
+	
 	<!-- Fav Icon  -->
-	<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-	<link rel="manifest" href="/manifest.json">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('') ?>/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('') ?>/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('') ?>/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('') ?>/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('') ?>/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('') ?>/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('') ?>/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('') ?>/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('') ?>/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="<?= base_url('') ?>/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('') ?>/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('') ?>/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('') ?>/favicon/favicon-16x16.png">
+	<link rel="manifest" href="<?= base_url('') ?>/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="msapplication-TileImage" content="<?= base_url('') ?>/favicon/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
+	
 	<!-- Site Title  -->
 	<title>SHAREit - Community Driven Project</title>
+	
 	<!-- Bundle and Base CSS -->
 	<link rel="stylesheet" href="<?= base_url('') ?>/assets/css/vendor.bundle.css?ver=1930">
 	<link rel="stylesheet" href="<?= base_url('') ?>/assets/css/style-shareit.css?ver=1930" id="changeTheme">
