9299fc6427368eb986bae07f8b83b25c95aa6365
Zahid Al Haris <zahidharis@gmail.com>
2022-01-01 22:16:54 +0700

    perbaikan 404 dan error page

--- app/Views/errors/html/production.php
+++ app/Views/errors/html/production.php
@@ -8,36 +8,177 @@ $tokenConfig = new BscTokenConfig();
 <html lang="en-us" class="js">
 
 <head>
-	<meta charset="utf-8">
-	<meta name="author" content="SHAREit">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-
-	<!-- Fav Icon  -->
-	<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('') ?>/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('') ?>/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('') ?>/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('') ?>/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('') ?>/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('') ?>/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('') ?>/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('') ?>/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('') ?>/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="<?= base_url('') ?>/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('') ?>/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('') ?>/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('') ?>/favicon/favicon-16x16.png">
-	<link rel="manifest" href="<?= base_url('') ?>/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="<?= base_url('') ?>/favicon/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
-
-	<!-- Site Title  -->
-	<title>SHAREit - Community Driven Project</title>
-
-	<!-- Bundle and Base CSS -->
-	<link rel="stylesheet" href="<?= base_url('') ?>/assets/css/vendor.bundle.css?ver=1930">
-	<link rel="stylesheet" href="<?= base_url('') ?>/assets/css/style-shareit.css?ver=1930" id="changeTheme">
+    <meta charset="utf-8">
+    <meta name="author" content="SHARE">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Share It is a community driven project with a share to earn program. All community members can participate by contributing to enlarge the coin by means of this coin to others. Community members who actively spread information about this coin have the opportunity to get prizes that will be drawn every week.
+          Share It has a goal for long-term investment, where investors can keep their coins for the long term and provide benefits to community members.
+          Share It will be one of the largest community-based projects to gather as many members as possible in the development of the project. Have a gradual and scheduled major marketing effort.">
+    <!-- Keyword -->
+    <meta name="keywords" content="cryptocurrency token logo, cryptocurrency token vs coin,
+cryptocurrency tokenomics,
+cryptocurrency token address,
+cryptocurrency token list,
+cryptocurrency tokens vs coins,
+cryptocurrency tokens list,
+cryptocurrency token meaning,
+cryptocurrency token definition,
+cryptocurrency token price,
+cryptocurrency token an overview,
+cryptocurrency and token,
+cryptocurrency basic attention token,
+difference between cryptocurrency and token,
+cryptocurrency and token difference,
+amazon cryptocurrency token,
+apple cryptocurrency token,
+create a cryptocurrency token,
+make a cryptocurrency token,
+what is a cryptocurrency token,
+whats a cryptocurrency token,
+how to launch a cryptocurrency token,
+taas (taas) is a cryptocurrency token,
+how to create a cryptocurrency token bnb,
+best cryptocurrency token,
+token based cryptocurrency,
+cryptocurrency token creation,
+cryptocurrency token calculator,
+cryptocurrency coin token,
+create cryptocurrency token,
+crypto token cryptocurrency,
+crypto token vs cryptocurrency,
+create cryptocurrency token free,
+cryptocurrency tokens,
+cryptocurrency token sales,
+cryptocurrency token types,
+crypto token definition,
+crypto token dsc,
+xrp token distribution,
+ethereum token decimals,
+crypto token distribution,
+crypto token drop,
+crypto dragons token,
+token ethereum,
+token ico,
+token defi adalah,
+ethereum token explorer,
+crypto token exchange,
+crypto token extension,
+ethereum token exchange,
+ethereum token erc20,
+crypto token example,
+crypto token economics,
+crypto token economy,
+crypto e token,
+bitcoin e token,
+cryptocurrency dan token,
+token for cryptocurrency,
+cryptocurrency feg token,
+free cryptocurrency token,
+facebook cryptocurrency token,
+cryptocurrency game token,
+cryptocurrency token is,
+cryptocurrency token ico,
+insurance cryptocurrency token,
+indian cryptocurrency token,
+crypto token key,
+cryptokitties token,
+crypto kicktoken,
+cryptokitty token,
+crypto king token,
+crypto token kit,
+xrp token kaufen,
+xrp token kurs,
+ethereum token list,
+crypto token list,
+crypto token logo,
+ethereum token limit,
+crypto token listing,
+cryptocurrency million token,
+make cryptocurrency token,
+crypto token news,
+crypto token new,
+crypto token nft,
+ethereum token nft,
+cryptocurrency new token,
+xrp new token,
+ethereum new token,
+ethereum network token,
+cryptocurrency and tokenization,
+cryptocurrency and tokens,
+cryptocurrency or token,
+ethereum.org token,
+crypto origin token,
+crypto one token,
+create ethereum token online,
+ethereum coin or token,
+cryptocurrency token presale,
+revolt token cryptocurrency price,
+crypto token que es,
+crypto token quantum,
+xrp quema tokens,
+crypto token o que é,
+xrp token que es,
+new cryptocurrency token release 2021,
+hp token cryptocurrency rewards codes,
+cryptocurrency tokens explained,
+cryptocurrency tokens available,
+cryptocurrency token swap,
+cryptocurrency security token,
+top cryptocurrency token,
+token bitcoin,
+crypto token utility,
+crypto token usb,
+crypto token unlock,
+xrp token utility,
+cryptocurrency utility token,
+cryptounit token,
+ethereum utility token,
+cryptoultraman token,
+cryptocurrency token value,
+ethereum token value,
+crypto token vs coins,
+crypto tokens and. coins,
+crypto vision token,
+cryptocurrency vs tokens,
+token dan crypto,
+v token crypto,
+ethereum vs tokens,
+cryptocurrency vs crypto token,
+crypto token website template,
+ethereum token wallet,
+crypto token with low supply,
+crypto token wallet,
+crypto token what is it,
+crypto token white paper,
+crypto token wikipedia,
+ethereum token what is it,
+new cryptocurrency token,
+x token cryptocurrency,
+xrp token yakacak,
+crypto tokens yet to launch,
+ethereum yield token,
+crypto zoo token,
+crypto zoon token,
+crypto zone token,
+cryptozoo token address,
+crypto zone token price,
+cryptozilla token,
+crypto zoo token buy,
+crypto zoo token price,
+z token crypto,
+bitcoin z token,
+coin erc20,
+new cryptocurrency token 2021">
+    <!-- Fav Icon  -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('') ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('') ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('') ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url('') ?>/site.webmanifest">
+    <!-- Site Title  -->
+    <title>SHARE Coin - Community Driven Project</title>
+    <!-- Bundle and Base CSS -->
+    <link rel="stylesheet" href="<?= base_url('') ?>/assets/css/vendor.bundle.css?ver=1930">
+    <link rel="stylesheet" href="<?= base_url('') ?>/assets/css/style-shareit.css?ver=1930" id="changeTheme">
 
 </head>
 
@@ -47,7 +188,7 @@ $tokenConfig = new BscTokenConfig();
 		<div class="container">
 			<main class="nk-pages nk-pages-centered tc-light px-0 text-center">
 				<header class="pt-5">
-					<a href="<?= route_to('homepage') ?>" class="d-inline-block logo-lg"><img class="error-logo" src="images/icons/shareit.png" srcset="images/icons/shareit.png 2x" alt="logo" /></a>
+					<a href="<?= route_to('homepage') ?>" class="d-inline-block logo-lg"><img class="error-logo" src="<?= base_url('') ?>/images/icons/shareit.png" srcset="<?= base_url('') ?>/images/icons/shareit.png 2x" alt="logo" /></a>
 				</header>
 				<div class="my-auto py-5">
 					<div class="row justify-content-center">
@@ -80,7 +221,8 @@ $tokenConfig = new BscTokenConfig();
 		</div>
 		<div class="nk-ovm shape-i ov-h"></div>
 	</div>
-	<div class="preloader"><span class="spinner spinner-round"></span></div>
+	<!-- preloader -->
+    <div class="preloader preloader-alt no-split"><span class="spinner spinner-alt"><img class="spinner-brand" src="<?= base_url('') ?>/images/icons/shareit.png" alt=""></span></div>
 
 	<!-- JavaScript -->
 	<script src="assets/js/jquery.bundle.js?ver=1930"></script>
