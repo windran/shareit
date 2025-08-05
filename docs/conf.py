22cd1811ddccc284a2be8cb6bc5631cc117872f1
Windra Nazarudin Azhar <saulertax@gmail.com>
2025-08-05 23:25:34 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,25 +1,13 @@
-import os
-import sys
 import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-sys.path.insert(0, os.path.abspath('.'))
-
-project = 'Share It Docs'
-copyright = '2025, Windran'
-author = 'Windran'
+project = 'ShareIt Docs'
+author = 'Windra'
 release = '1.0'
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
-]
-
+extensions = ['sphinx_rtd_theme']
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
