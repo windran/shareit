77ad2a66e6bb6cbbe44873ae64c4e3621cfe0775
Windra Nazarudin Azhar <saulertax@gmail.com>
2025-08-05 23:20:48 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,20 @@
-
-# Configuration file for the Sphinx documentation builder.
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
-# -- Project information -----------------------------------------------------
-project = 'Share It'
-copyright = '2025, Windra'
-author = 'Windra Nazarudin Azhar'
-release = '0.1'
+project = 'Share It Docs'
+copyright = '2025, Windran'
+author = 'Windran'
+release = '1.0'
 
-# -- General configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
